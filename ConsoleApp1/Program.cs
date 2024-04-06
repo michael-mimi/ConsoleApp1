48ffbc2418b51c618f120914d776a78a89e951a1
michael-mimi <michenfei36@hotmail.com>
2024-04-06 22:00:39 +0800

    添加项目文件。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿
+namespace Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           List<int> even = new List<int>();
+            List<int> odd = new List<int>();
+            for (int i = 0; i <=20; i++)
+            {
+                if (i%2==0)
+                {
+                    even.Add(i);
+                }
+                else
+                {
+                    odd.Add(i);
+                }
+            }
+            foreach (var item in even)
+            {
+                Console.Write($"{item} ");
+            }
+            //Console.WriteLine(Environment.NewLine + "Here are the odd numbers:");
+            foreach (var item in odd)
+            {
+                Console.Write($"{item} ");
+            }
+        }
+    }
+}
